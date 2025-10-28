6f80c3527c6ae27fd200252c522eb7c34b991fa3
mariacarolinass <m.carolina.soares1@gmail.com>
2025-10-28 17:45:49 -0300

    removi informações sem uso

--- apps/students/serializers.py
+++ apps/students/serializers.py
@@ -5,14 +5,12 @@ from apps.students.models import Student
 class StudentSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(source='user.email', read_only=True)
     username = serializers.CharField(source='user.username', read_only=True)
-    full_name = serializers.CharField(source='user.get_full_name', read_only=True)
 
     class Meta:
         model = Student
         fields = [
             'id',
             'username',
-            'full_name',
             'email',
             'bio',
             'is_active',
