50386ca251f6cd2c755a9fa51e5caee2779586ca
mariacarolinass <m.carolina.soares1@gmail.com>
2025-10-29 16:40:52 -0300

    atualizei rotas

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path, include
 from apps.users.views import RegisterView, LogoutView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.users.views import (
+    RegisterView,
+    LoginView,
+    CustomTokenRefreshView,
+    LogoutView,
+    SocialAuthInfoView,
+    UserAdminCreateView
+)
 
 urlpatterns = [
-    path('auth', include('social_django.urls', namespace='social')),
-    path('auth/register', RegisterView.as_view(), name='register'),
-    path('auth/login', TokenObtainPairView.as_view(), name='login'),
-    path('auth/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path("auth/logout", LogoutView.as_view(), name="logout"),
+    path("auth/social/", include("social_django.urls", namespace="social")),
+    path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/login/", LoginView.as_view(), name="login"),
+    path("auth/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/logout/", LogoutView.as_view(), name="logout"),
+    path("auth/social/info/", SocialAuthInfoView.as_view(), name="social_info"),
+    path('admin/users/create/', UserAdminCreateView.as_view(), name='admin-create-user'),
 ]
