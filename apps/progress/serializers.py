8b8c10f259fc801e266aad4b4751eb30dd83fe30
mariacarolinass <m.carolina.soares1@gmail.com>
2025-10-29 16:47:20 -0300

    corrigi selializer

--- apps/progress/serializers.py
+++ apps/progress/serializers.py
@@ -3,20 +3,17 @@ from apps.progress.models import Progress
 
 
 class ProgressSerializer(serializers.ModelSerializer):
-    student_name = serializers.CharField(source='student.get_full_name', read_only=True)
-    lesson_title = serializers.CharField(source='lesson.title', read_only=True)
-    course_title = serializers.CharField(source='course.title', read_only=True)
+    student = serializers.StringRelatedField(many=False, read_only=True)
+    lesson = serializers.StringRelatedField(many=False, read_only=True)
+    course = serializers.StringRelatedField(many=False, read_only=True)
 
     class Meta:
         model = Progress
         fields = [
             'id',
             'student',
-            'student_name',
             'course',
-            'course_title',
             'lesson',
-            'lesson_title',
             'status',
             'created_at',
             'updated_at'
