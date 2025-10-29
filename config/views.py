f1a4312f310d607773ed2ce40b7c5fbe61db6d7c
mariacarolinass <m.carolina.soares1@gmail.com>
2025-10-29 16:36:44 -0300

    add serializer vázio

--- config/views.py
+++ config/views.py
@@ -1,11 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import serializers, status
-from rest_framework.permissions import AllowAny
-
-
-class EmptySerializer(serializers.Serializer):
-    pass
+from rest_framework import status
+from apps.users.serializers import EmptySerializer
 
 
 class IndexView(APIView):
@@ -13,7 +9,6 @@ class IndexView(APIView):
     Endpoint público de boas-vindas à API AluConnect.
     """
     serializer_class = EmptySerializer
-    permission_classes = [AllowAny]
 
     def get(self, request):
         return Response({"message": "Welcome to AluConnect API"}, status=status.HTTP_200_OK)
