e9ad119fb48a52d4ba00a6acdcbb41d8956b37d8
mariacarolinass <m.carolina.soares1@gmail.com>
2025-11-04 09:03:26 -0300

    criei rota para auth/social/jwt

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from apps.users.views import RegisterView, LogoutView
 from apps.users.views import (
+    SocialJWTCallbackView,
     RegisterView,
     LoginView,
     CustomTokenRefreshView,
@@ -11,6 +12,7 @@ from apps.users.views import (
 
 urlpatterns = [
     path("auth/social/", include("social_django.urls", namespace="social")),
+    path("auth/social/jwt/", SocialJWTCallbackView.as_view(), name="social_jwt_callback"),
     path("auth/register/", RegisterView.as_view(), name="register"),
     path("auth/login/", LoginView.as_view(), name="login"),
     path("auth/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
